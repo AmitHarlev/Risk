fa1a6282cf1d82c5d5ea9ff584b3c2d9f8e1c85d
Amit Harlev <amit.harlev@gmail.com>
2018-12-29 22:59:11 -0800

    messing around with socket io

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 from color import Color
 from player import Player
 
@@ -11,16 +11,25 @@ socketio = SocketIO(app)
 if __name__ == '__main__':
     socketio.run(app)
 
-unusedPlayers = set()
-players = set()
+# unusedPlayers = set()
+# players = set()
 
 @app.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/newPlayer')
-def newPlayer():
-    global test
-    player = Player(Color.RED)
-    return str(test)
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    emit('received message' + str(json))
+
+@socketio.on('start')
+def handle_start(data):
+    print(data)
+    emit("begin", broadcast=True)
+
+# @app.route('/newPlayer')
+# def newPlayer():
+#     global test
+#     player = Player(Color.RED)
+#     return str()
     
