1abe4bdc9cf6ee40d4e66f8453e6e68f46b43809
Amit Harlev <amit.harlev@gmail.com>
2018-12-29 23:29:07 -0800

    added some game state to game

--- game.py
+++ game.py
@@ -1,4 +1,13 @@
+from map import Map
+
 class Game:
 
-    def __init__(self):
-        pass
+    def __init__(self, numPlayers):
+        self.__numPlayers = numPlayers
+        self._map = Map()
+        self._turn = None
+        self._turnOrder = []
+    
+
+    def nextTurn(self):
+        self._turn = self._turnOrder[self._turnOrder.index(self._turn)+1]
